8c935afa599b9dc1cee7bcdf7051e1d21066a6bf
root <root@LAPTOP-S8CROK7P.localdomain>
2025-06-18 17:22:26 +0000

    start learning python with primitive types

---
+++ 01 Primitive Types/01_string.py
@@ -0,0 +1,23 @@
+firstName = "Jeetu"
+lastName = "Dewangan"
+fullName = F"{len(firstName)} {len(lastName)}"
+message = " Hi \'Jeetu\', How are you? "
+courseName = 'python programming'
+# print(fullName)
+# print(message)
+# print(firstName.upper())
+# print(firstName.lower())
+# print(message.strip())
+# print(message.lstrip())
+# print(message.rstrip())
+# print(message.split())    # By default split by dash(" ")
+# print(courseName.title())
+# print(courseName.capitalize())
+# print(courseName.count('o'))
+# print(courseName.index('p'))
+# print("Jeetu".index('J'))
+# print(courseName.find('pr'))
+# print("jeetu".find('J'))
+# print(courseName.replace('p', 'P'))
+# print('py' in courseName)
+# print('java' not in courseName)
---
+++ 01 Primitive Types/02_number.py
@@ -0,0 +1,46 @@
+# import math
+# number data type and numberic operator
+# x = 1
+# y = 1.1
+# z = 2 + 3j  # a+ bi
+# print(z)
+# print(3+9)
+# print(4-2)
+# print(3*2)
+# print(5/2)
+# print(5//2)
+# print(5 % 2)
+# print(2**3)
+
+# x += 4
+# print(x)
+# x -= 1
+# print(x)
+# x *= 2
+# print(x)
+# print(x == y)
+
+# number functions
+
+# print(round(y))
+# print(abs(-2.5))
+
+# print(math.ceil(y))
+# print(math.floor(y))
+# print(math.sqrt(4))
+
+# Type Conversion
+# int()
+# float()
+# str()
+# bool()
+
+# Falsy Value
+# ""
+# 0
+# None
+# False
+x = int(input("x: "))
+y = x + 1
+print(y)
+print(type(x))
